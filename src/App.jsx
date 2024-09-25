6fc90d121179dd27ef6c45a40ddd384bea9aab03
Sunil Mishra <sunilmishra.php@outlook.com>
2024-09-25 21:47:30 +0530

    create screens and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
+import AboutScreen from './screens/AboutScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
-const App = () => {
-  return <div className='text-red-500'>App</div>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/sign-in" element={<SigninScreen />} />
+        <Route path="/sign-up" element={<SignupScreen />} />
+        <Route path="/about" element={<AboutScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
