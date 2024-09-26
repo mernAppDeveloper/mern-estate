02970081a0f4d8e219391fa88eecd24c0a88f5c0
Sunil Mishra <sunilmishra.php@outlook.com>
2024-09-26 09:16:22 +0530

    connect to DB

--- api/server.js
+++ api/server.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log('Connected to mongoDB!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
-})
+app.listen(5000, () => {
+  console.log('Server is running on port 5000!');
+});
