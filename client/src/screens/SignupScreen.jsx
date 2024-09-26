19776e183f430321cfacfddbfc9c9f5c92294c42
Sunil Mishra <sunilmishra.php@outlook.com>
2024-09-26 16:06:33 +0530

    complete signup page functionality

--- client/src/screens/SignupScreen.jsx
+++ client/src/screens/SignupScreen.jsx
@@ -1,16 +1,57 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignupScreen() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOnchange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const dataResponse = await response.json();
+      if (dataResponse.success === false) {
+        setLoading(false);
+        setError(dataResponse.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sig-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           name=""
           id="username"
           placeholder="username"
           className="border p-3 rounded-lg"
+          onChange={handleOnchange}
         />
         <input
           type="email"
@@ -18,6 +59,7 @@ export default function SignupScreen() {
           id="email"
           placeholder="email"
           className="border p-3 rounded-lg"
+          onChange={handleOnchange}
         />
         <input
           type="password"
@@ -25,14 +67,21 @@ export default function SignupScreen() {
           id="password"
           placeholder="password"
           className="border p-3 rounded-lg"
+          onChange={handleOnchange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? 'loading...' : 'Sign up'}
         </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to={'/sign-in'}><span className='text-blue-700'>Sign in</span>></Link>
+        <Link to={'/sign-in'}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
       </div>
     </div>
   );
