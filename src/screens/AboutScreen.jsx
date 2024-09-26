bbde00d84c6ec9f2f54ed3822eb745646c956d86
Sunil Mishra <sunilmishra.php@outlook.com>
2024-09-26 07:30:57 +0530

    create and run server

--- src/screens/AboutScreen.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function AboutScreen() {
-  return <div>AboutScreen</div>;
-}
--- src/screens/ProfileScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProfileScreen(){
-  return (
-    <div>
-      ProfileScreen
-    </div>
-  )
-}
